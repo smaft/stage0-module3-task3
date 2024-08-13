a6abe9a7757ba5285c7dbf9797db32782416d291
litim smail <litimsmail@gmail.com>
2024-08-13 14:59:20 +0100

    UpDate Task3

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -3,10 +3,12 @@ package school.mjc.stage0.module3.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
-        int first, second, third;
-        first = 1;
-        second = 10;
-        third = 100;
-        System.out.println("first\nsecond\nthird");
+
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,18 +3,27 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
-        int first, second, third;
-        first = 1;
-        second = 10;
-        third = 100;
-        System.out.println("first\nsecond\nthird");
-         int linkToFirst = first;
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        int linkToFirst = first;
         int linkToSecond = second;
         int linkToThird = third;
+
         first = 5;
         second = 6;
         third = 4;
-        System.out.println("linkToFirst\nlinkToSecond\nlinkToThird\nfirst\nsecond\nthird");
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
 
 
     }
